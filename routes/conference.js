4bc0b6058456199688a12ce9bea9133788565720
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-11-15 03:47:14 -0600

    added all files

--- routes/conference.js
+++ routes/conference.js
@@ -1,9 +1,29 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('conference', { title: 'Conference' });
+// });
+
+// module.exports = router;
+
 var express = require('express');
 var router = express.Router();
+var conference_controller = require('../controllers/conference');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('conference', { title: 'Conference' });
-});
+router.get('/', conference_controller.conference_list);
+
+router.post('/create', conference_controller.conference_create_post);
+
+router.get('/:id', conference_controller.conference_detail);
+
+// router.get('/:id/update', conference_controller.conference_update_get);
+
+// router.get('/:id/delete', conference_controller.conference_delete_get);
+
+// router.post('/:id/update', conference_controller.conference_update_post);
+
+// router.post('/:id/delete', conference_controller.conference_delete_post);
 
 module.exports = router;
