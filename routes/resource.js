66dbd1121f43a94d97bbb1f318a77266ee327c30
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-11-15 03:47:04 -0600

    added all files

--- routes/resource.js
+++ routes/resource.js
@@ -1,20 +1,40 @@
+// var express = require('express');
+// var router = express.Router();
+// // Require controller modules.
+// var api_controller = require('../controllers/api');
+// var conference_controller = require('../controllers/conference');
+// /// API ROUTE ///
+// // GET resources base.
+// router.get('/', api_controller.api);
+// /// conference ROUTES ///
+// // POST request for creating a conference.
+// router.post('/conference', conference_controller.conference_create_post);
+// // DELETE request to delete conference.
+// router.delete('/conference/:id', conference_controller.conference_delete);
+// // PUT request to update conference.
+// router.put('/conference/:id', conference_controller.conference_update_put);
+// // GET request for one conference.
+// router.get('/conference/:id', conference_controller.conference_detail);
+// // GET request for list of all conference items.
+// router.get('/conference', conference_controller.conference_list);
+// module.exports = router;
+
 var express = require('express');
 var router = express.Router();
-// Require controller modules.
+
 var api_controller = require('../controllers/api');
 var conference_controller = require('../controllers/conference');
-/// API ROUTE ///
-// GET resources base.
+
 router.get('/', api_controller.api);
-/// conference ROUTES ///
-// POST request for creating a conference.
+
 router.post('/conference', conference_controller.conference_create_post);
-// DELETE request to delete conference.
+
 router.delete('/conference/:id', conference_controller.conference_delete);
-// PUT request to update conference.
+
 router.put('/conference/:id', conference_controller.conference_update_put);
-// GET request for one conference.
+
 router.get('/conference/:id', conference_controller.conference_detail);
-// GET request for list of all conference items.
+
 router.get('/conference', conference_controller.conference_list);
+
 module.exports = router;
